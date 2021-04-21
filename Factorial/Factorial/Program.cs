6ac08e5fcbbd2c80a4ac5516aba8989f2ab9e665
shubhambodake04 <shubhambodake6476@gmail.com>
2021-04-21 10:09:27 +0530

    [Shubham] added Factorial

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static int factorial(int n)
+        {
+            if (n <= 1)
+                return 1;
+
+           return n * factorial(n-1);
+        }
+
+        static void Main(string[] args)
+        {
+            int number, result;
+            Console.WriteLine("Enter a non negative number:" );
+            number=Convert.ToInt32(Console.ReadLine());
+            result = factorial(number);
+            Console.WriteLine("{0}!={1}",number,result);
+        }
+    }
+}
---
+++ Factorial/FactorialUsingNormal/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FactorialUsingNormal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, number, result;
+            Console.WriteLine("Enter a non negative number:");
+            number=Convert.ToInt32(Console.ReadLine());
+            result = number;
+            for(i=number-1; i>=1; i--)
+            {
+                result = result * i;
+            }
+            Console.WriteLine("Factorial of given number is : " +result);
+            Console.ReadLine();
+        }
+    }
+}
